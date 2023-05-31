28198a0e8e323fc827faa8ed353590c50ea7c7a3
gerald <gerald.hoff@wsu.edu>
2023-05-30 17:00:02 -0700

    added decaying_dimer.jl

--- examples/decaying_dimer.jl
+++ examples/decaying_dimer.jl
@@ -1,18 +1,34 @@
 using Gillespie
 import Random: seed!
 
+# Function dealing with state x and parameters parms
+# **F** : a `Function` or a callable type, which itself takes two arguments; x, a `Vector` of `Int64` representing the states, and parms, a `Vector` of `Float64` representing the parameters of the system. In the case of time-varying rates (for algorithms `:jensen` and `:tjm`), there should be a third argument, the time of the system.
 function F_dd(x,parms)
     (S1,S2,S3) = x
     (c1,c2,c3,c4) = parms
     [c1*S1,c2*S1*S1,c3*S2,c4*S2]
 end
 
+# a `Vector` of `Int64`, representing the initial states of the system.
 x0 = [10000,0,0]
+
+# a `Matrix` of `Int64`, representing the transitions of the system, organised by row.
 nu = [[-1 0 0];[-2 1 0];[2 -1 0];[0 -1 1]]
+
+# a `Vector` of `Float64` representing the parameters of the system.
 parms = [1.0,0.002,0.5,0.04]
+
+# the final simulation time (`Float64`)
 tf = 10.0
 seed!(1234)
 
+"
+There are several named arguments:
+
+- **algo**: the algorithm to use (`Symbol`, either `:gillespie` (default), ':jensen', or ':tjm').
+- **max_rate**: the maximum rate (`Float64`, for Jensen's method only).
+- **thin**: (`Bool`) whether to thin jumps for Jensens method (default: `true`).
+"
 result = ssa(x0,F_dd,nu,parms,tf)
 
 data = ssa_data(result)
--- src/ModelParser.jl
+++ src/ModelParser.jl
@@ -1,5 +1,5 @@
 #Looks for file_name in pysces_models folder and returns a string with the file's contents
-function getModelString(file_name)
+function getModelString(file_name::String)
     buffer = IOBuffer()
 
     open(pwd() * "/pysces_models/" * file_name) do f
@@ -9,11 +9,16 @@ function getModelString(file_name)
     
         # read till end of file
         while ! eof(f) 
-    
-        # read a new / next line for every iteration          
-        s = readline(f)         
-        line += 1
-        println(buffer, "$line . $s")
+            # read a new / next line for every iteration          
+            s = readline(f)         
+            line += 1
+            first = findfirst("#", s)
+            if (first === nothing)
+                println(buffer, "$line . $s")
+            else
+                s = s[begin:first[1]-1]
+                println(buffer, "$line . $s")
+            end
         end
     
     end
