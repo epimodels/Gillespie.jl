9bd6076dc1b6faa689339a157481db408ddab461
gerald <gerald.hoff@wsu.edu>
2023-06-01 14:52:39 -0700

    gets Nmatrix and converts to Gillespie.jl format

--- src/Parser.jl
+++ src/Parser.jl
@@ -1,8 +1,23 @@
 using PyCall
 const pysces = pyimport("pysces") #import pysces
+using Gillespie
+import Random: seed!
 
-model = pysces.model("DecayingDimerizing", dir="c:\\Pysces\\psc")
-species = model.species
-reactions = model.reactions
-matrix = model.Nmatrix
-model.showN()
+model = pysces.model("DecayingDimerizing", dir="c:\\Pysces\\psc") #PyObject
+# species = model.species #Tuple{String, String, String}
+# reactions = model.reactions #Tuple{4, String}
+# parameters = model.parameters
+# println(parameters)
+# println(species)
+# println(reactions)
+# print(model.k1)
+
+#Stoichiometric matrix
+#get the Nmatrix using pysces, convert it to an array, then to an integer array, then find its transpose
+nu = (convert(Matrix{Int64}, model.Nmatrix.array))'
+
+# model.showN()
+
+# the final simulation time (`Float64`)
+# tf = 10.0
+# seed!(1234)
