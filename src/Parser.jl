248e73bb38f41275f669380aaa27d49cd44bb2e3
gerald <gerald.hoff@wsu.edu>
2023-06-01 17:55:30 -0700

    gets initial states of system

--- src/Parser.jl
+++ src/Parser.jl
@@ -4,17 +4,26 @@ using Gillespie
 import Random: seed!
 
 model = pysces.model("DecayingDimerizing", dir="c:\\Pysces\\psc") #PyObject
-# species = model.species #Tuple{String, String, String}
-# reactions = model.reactions #Tuple{4, String}
-# parameters = model.parameters
-# println(parameters)
-# println(species)
-# println(reactions)
+species = model.species #Tuple{String, String, String}
+reactions = model.reactions #Tuple{4, String}
+parameters = model.parameters
+println(parameters)
+println(species)
+println(reactions)
 # print(model.k1)
 
-#Stoichiometric matrix
-#get the Nmatrix using pysces, convert it to an array, then to an integer array, then find its transpose
-nu = (convert(Matrix{Int64}, model.Nmatrix.array))'
+# a `Vector` of `Int64`, representing the initial states of the system. i.e. states of S1, S2, S3. #InitVar
+x1 = Vector{Int64}([])
+for i in species
+    append!(x1, convert(Int64, eval(Meta.parse("model." * i))))
+end
+
+print(typeof(x1))
+print(x1)
+
+# Stoichiometric matrix
+# get the Nmatrix using pysces, convert it to an array, then to an integer array, then find its transpose
+# nu = (convert(Matrix{Int64}, model.Nmatrix.array))'
 
 # model.showN()
 
